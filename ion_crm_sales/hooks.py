a380ee94c77a9b57642a4c7b33d5d43f741217a2
Luxoruus <meddhiaajaziri@gmail.com>
2025-12-19 20:14:40 +0100

    commission working on

--- ion_crm_sales/hooks.py
+++ ion_crm_sales/hooks.py
@@ -50,8 +50,17 @@ doc_events = {
         "after_insert": "ion_crm_sales.ion_crm_sales.doc_events.distributor_handlers.create_sales_partner_for_distributor"
     },
     "Sales Invoice": {
-        "on_submit": "ion_crm_sales.ion_crm_sales.doc_events.sales_invoice_handlers.on_submit",
-        "on_change": "ion_crm_sales.ion_crm_sales.doc_events.sales_invoice_handlers.on_change"
+        # "on_submit": "ion_crm_sales.ion_crm_sales.doc_events.sales_invoice_handlers.on_submit",
+        # "on_change": "ion_crm_sales.ion_crm_sales.doc_events.sales_invoice_handlers.on_change"
+
+        "on_submit": "ion_crm_sales.commission.triggers.on_sales_invoice_submit",
+        "on_cancel": "ion_crm_sales.commission.triggers.on_sales_invoice_cancel",
+        # optional: when amended (new version from previous), recompute as needed
+        "on_update_after_submit": "ion_crm_sales.commission.triggers.on_sales_invoice_update_after_submit",
+    },
+    
+    "Payment Entry": {
+        "on_submit": "ion_crm_sales.commission.triggers.on_payment_entry_submit"
     }
 }
 
---
+++ ion_crm_sales/ion_crm_sales/doctype/sales_target_and_commission_sheet/sales_target_and_commission_sheet.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, ard.ly and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sales Target and Commission Sheet", {
+// 	refresh(frm) {
+
+// 	},
+// });
